0b31bf66cd8d386e97b918e7acdfc06eeb9031cf
pattyouwehand <pattyouwehand@gmail.com>
2019-09-14 12:14:32 +0200

    setup db

---
+++ db.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize')
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const db = new Sequelize(databaseUrl)
+
+
+module.exports = db
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = 4000
+
+db
+  .sync({ force: false })
+  .then(() => console.log('Database schema updated'))
+  .catch(console.error)
 
 app.get('/test', (req, res) => res.send('Hello test!'))
 app.listen(port, () => console.log(`Listening to port ${port}`))
