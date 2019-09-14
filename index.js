dc1348e9b72b763766ca647bba7210796878fe22
pattyouwehand <pattyouwehand@gmail.com>
2019-09-14 13:29:39 +0200

    setup router

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
-
-const db = require('./db')
-const Image = require('./image/model')
 const app = express()
 
 const port = 4000
+const db = require('./db')
+
+const Image = require('./image/model')
+const imageRouter = require('./image/router')
+
 
 db
   .sync({ force: false })
   .then(() => console.log('Database schema updated'))
   .catch(console.error)
 
+app.use(imageRouter)
 app.get('/test', (req, res) => res.send('Hello test!'))
 app.listen(port, () => console.log(`Listening to port ${port}`))
